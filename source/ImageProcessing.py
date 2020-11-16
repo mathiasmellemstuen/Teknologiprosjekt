705174625a762f690cea837314feebb7e43989de
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2020-11-16 14:12:25 +0100

    Testing

--- source/ImageProcessing.py
+++ source/ImageProcessing.py
@@ -1,5 +1,29 @@
-import picamera
+from io import BytesIO
+from time import sleep
+from picamera import PiCamera
 import numpy
 import cv2
+from datetime import datetime
+#warmup = 2 # In seconds
+#print("Staring image processing module.")
 
-print("Staring image processing module.")
+#stream = BytesIO() 
+#camera = PiCamera() 
+#camera.start_preview()
+
+#print("Camera warmup: starting")
+#sleep(warmup)
+#print("Camera warmup: finished")
+
+#camera.capture(stream, 'jpeg')
+
+
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview()
+
+sleep(2)
+
+print("TIME BEFORE:", datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
+camera.capture("/var/www/control-panel/capture.jpg")
+print("TIME AFTER:", datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
