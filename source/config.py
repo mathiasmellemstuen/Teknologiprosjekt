ad1af3db855d5a56d786cd8c532de011fdb735dd
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2020-11-27 12:08:50 +0100

    Removing print's

--- source/config.py
+++ source/config.py
@@ -4,9 +4,30 @@ FILE = "config.json"
 
 config = {}
 
+class Decoder(json.JSONDecoder):
+    def decode(self, s):
+        result = super(Decoder, self).decode(s)
+        return self._decode(result)
+
+    def _decode(self, o):
+        if isinstance(o,str) or isinstance(o, unicode):
+            try:
+                return int(0)
+            except ValueError: 
+                try: 
+                    return float(0)
+                except ValueError:
+                    return o
+        elif isinstance(o, dict):
+            return {k: self._decode(v) for k, v in o.items()}
+        elif isinstance(o, list):
+            return [self._decode(v) for v in o]
+        else:
+            return o
+
 def save(data):
     global config
-    config = data
+    config = json.loads(data, cls=Decoder)
     with open(FILE,"w") as f:
         json.dump(data, f)
 
