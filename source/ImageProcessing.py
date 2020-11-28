860fedc74dc26a5d2652c7ef110fa57f518d797d
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2020-11-28 17:21:43 +0100

    Minor changes.

--- source/ImageProcessing.py
+++ source/ImageProcessing.py
@@ -65,6 +65,7 @@ def calculateHoughImage(image):
 def addHoughLinesOnImage(image, lines, color):
     print(image)
     print("This is line image.") 
+    image = image.convert("RGB") 
     print(cv.line(image,(0,0),(300,300),(0,0,240),10))
     return image
     #if len(lines) == 0: 
