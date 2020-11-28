c0e22ed368cba2acb7836f0fba872fded78f7d55
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2020-11-28 17:37:40 +0100

    Minor changes.

--- source/ImageProcessing.py
+++ source/ImageProcessing.py
@@ -71,19 +71,20 @@ def addHoughLinesOnImage(image, lines, color):
     if len(lines) == 0: 
         print("Could not add hough lines. Returning.")
         return image
-
-    for rho, theta in lines[0]:
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a * rho
-        y0 = b * rho
-        x1 = int(x0 + 1000*(-b))
-        y1 = int(y0 + 1000*(a))
-        x2 = int(x0 - 1000*(-b))
-        y2 = int(y0 - 1000*(a))
-        print("Creating a line between x1:", x1, ", y1:",y1,", x2:",x2,", y2:",y2)
-        image = cv.cvtColor(image, cv.COLOR_GRAY2BGR)
-        image = cv.line(image, (x1,y1), (x2,y2), color, 2)
+    
+    for line in lines: 
+        for rho, theta in line:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + 1000*(-b))
+            y1 = int(y0 + 1000*(a))
+            x2 = int(x0 - 1000*(-b))
+            y2 = int(y0 - 1000*(a))
+            print("Creating a line between x1:", x1, ", y1:",y1,", x2:",x2,", y2:",y2)
+            image = cv.cvtColor(image, cv.COLOR_GRAY2BGR)
+            image = cv.line(image, (x1,y1), (x2,y2), color, 2)
 
     return image
 
