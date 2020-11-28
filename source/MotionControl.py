06fa7e837036761f8b04b57dff767419d5d4a1d4
Tobias Hallingstad <tobias@hallingstad.me>
2020-11-28 19:06:15 +0100

    Started work on Motion control

--- source/MotionControl.py
+++ source/MotionControl.py
@@ -1,3 +1,32 @@
 from easygopigo3 import EasyGoPiGo3
+import config
+
+defaltSpeed = 0
+defaultTurn = 0
+
+speed = 0   # This is the speed of the robot       |Int     | -100 -> 100
+turn = 0    # This is the turning rate of the robo |Float   | -1 -> 1
 
 robot = EasyGoPiGo3()
+
+def getSpeed():
+    return config.load()["speed"]
+
+def getAngle():
+    return config.load()["rotation"]
+
+def setVelosity():
+    global speed, turn, robot
+    
+    try:
+        speed = getSpeed()
+        turn = getTurn()
+        break
+    except:
+        print("Did not load speed and turn, stopping for this loop")
+        speed = defaultSpeed
+        turn = defaultTurn 
+
+    turn = [speed * turn, speed * (turn * -1)]
+
+    robot.steer(turn[0], turn[1])
