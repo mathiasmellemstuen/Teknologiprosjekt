d4b23fcc503ef48b9c80f537c05f89ea4e62e4c4
Tobias Hallingstad <tobias@hallingstad.me>
2020-11-29 15:06:08 +0100

    removed some lines of code

--- source/MotionControl.py
+++ source/MotionControl.py
@@ -5,13 +5,10 @@ import threading
 defaltSpeed = 0
 defaultTurn = 0
 
-
-speed = 0   # This is the speed of the robot       |Int     | -100 -> 100
-turn = 0    # This is the turning rate of the robot|Float   | -1 -> 1
+speed = 0   # This is the speed of the robot        |Int     | -100 -> 100
+turn = 0    # This is the turning rate of the robot |Float   | -1 -> 1
 
 thread = None
-speed = 10   # This is the speed of the robot       |Int     | -100 -> 100
-turn = 0    # This is the turning rate of the robo |Float   | -1 -> 1
 
 robot = EasyGoPiGo3()
 
@@ -29,17 +26,13 @@ def setVelosity():
 
     try:
         speed = getSpeed()
-        turn = getAngle()
-        speed = 10# getSpeed()
-        turn = 0# getTurn()
+        turn = getTurn()
     except:
         print("Did not load speed and turn, stopping for this loop")
         speed = defaultSpeed
         turn = defaultTurn 
 
     turn = [speed * turn, speed * (turn * -1)]
-
-    print(turn)
 
 def start():    # Function to start the robot
     global thread
