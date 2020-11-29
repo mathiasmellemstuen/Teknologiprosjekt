ef98f7fb944f28745d63e28b0d2946c27b584ed3
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2020-11-29 15:57:51 +0100

    Fixed syntax error.

--- source/MotionControl.py
+++ source/MotionControl.py
@@ -6,47 +6,46 @@ defaultSpeed = 0
 defaultTurn = 0
 
 speed = 0   # This is the speed of the robot        |Int     | -100 -> 100
-turn = 0    # This is the turning rate of the robot |Float   | -1 -> 1
+rotation = 0    # This is the turning rate of the robot |Float   | -1 -> 1
 
 thread = None
 
 robot = EasyGoPiGo3()
 
 def getSpeed():
+    global speed
     speed = config.load()["speed"]
     return speed
 
-def getAngle():
+def getRotation():
+    global rotation 
     rotation = config.load()["rotation"]
     return rotation
 
 def setVelosity():
-    global speed, turn, robot
+    global speed, rotation, robot
     global defaultSpeed, defaultTurn
-    
-    while True:
-        try:
-            speed = getSpeed()
-            turn = getTurn()
-        except:
-            print("Did not load speed and turn, stopping for this loop")
-            speed = defaultSpeed
-            turn = defaultTurn 
 
-        turn = [speed * turn, speed * (turn * -1)]
+    try:
+        speed = getSpeed()
+        rotation = getRotation()
+    except:
+        print("Did not load speed and turn, stopping for this loop")
+        speed = defaultSpeed
+        rotation = defaultTurn 
+
+    rotation = [speed * rotation, speed * (rotation * -1)]
 
 def start():    # Function to start the robot
     global thread
 
     thread = threading.Thread(target = setVelosity)
-    thread.start()
-
 
 def stop():   # Function that is called when the script ends
-    global speed, turn, thread
-    
-    robot.stop()
-    thread.join()
+    global speed, rotation, thread
 
     speed = 0
-    turn = 0
+    rotation = 0
+
+    robot.stop()
+    thread.join()
