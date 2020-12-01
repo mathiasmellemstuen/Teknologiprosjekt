ff001a0af3dab8604cb3e9155510eb7e0b22ce8a
Mathias <mathias.mellemstuen@gmail.com>
2020-12-01 15:07:38 +0100

    Testing

--- source/ImageProcessing.py
+++ source/ImageProcessing.py
@@ -86,10 +86,10 @@ def calculateNodes(houghLines, width, height):
     for line in houghLines: 
         for x1,y1,x2,y2 in line: 
             lineAngle = calculateLineAngle(x1,y1,x2,y2)
- 
+            print("inside houghlines") 
             if (lineAngle >= (math.pi / 4) and lineAngle <= ((3*math.pi)/4)) or (lineAngle >= math.pi + (math.pi/4) and lineAngle <= ((2*math.pi) - (math.pi/4))):
                 # Vertical search
-                print("Inside angles.")
+                #print("Inside angles.")
                 for y in range(y1,y2,step):
                     for line2 in houghLines:
                         if line2 is not line:
