e709b2c4ac574004d89decf156e2aff7d4ef7579
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2020-12-01 11:52:21 +0100

    Fixed syntax error

--- source/ImageProcessing.py
+++ source/ImageProcessing.py
@@ -94,7 +94,7 @@ def calculateNodes(houghLines, width, height):
                                     x = (x2 - x1) + x1 if x2 >= x1 else (x1 - x2) + x2
                                     a = (a2 - a1) + a1 if a2 >= a1 else (a1 - a2) + a2
                                     xPos = ((a - x) / 2) + x if a >= x else ((x - a) / 2) + a
-                                    print("x:",x,"a:",a,"xPos:",xPos "with angle:",lineAngle)  
+                                    print("x:",x,"a:",a,"xPos:",xPos, "with angle:",lineAngle)  
                                     element = {"x":xPos, "y": y}
 
                                     if element not in nodes:
